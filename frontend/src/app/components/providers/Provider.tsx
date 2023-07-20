00bfcd06a5aa9cdc7f349979ebe2cb778d831bc7
satoshi02198 <awareness_yoga@yahoo.co.jp>
2023-07-20 22:36:24 +0900

    202307202236

--- frontend/src/app/components/providers/Provider.tsx
+++ frontend/src/app/components/providers/Provider.tsx
@@ -14,7 +14,7 @@ import React, {
   useMemo,
 } from "react";
 import { ethers } from "ethers";
-import contractAddress from "../../contracts/contract-address-localhost.json";
+import contractAddress from "../../contracts/contract-address-sepolia.json";
 import transfusionArtifact from "../../contracts/Transfusion.json";
 
 type ProviderProps = {
