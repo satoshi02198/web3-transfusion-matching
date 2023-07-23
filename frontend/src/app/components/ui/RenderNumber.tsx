66649570254d29d593845076baa80953ab32a46b
satoshi02198 <awareness_yoga@yahoo.co.jp>
2023-07-23 11:56:46 +0900

    202307231156

---
+++ frontend/src/app/components/ui/RenderNumber.tsx
@@ -0,0 +1,18 @@
+import React, { Children } from "react";
+
+type RenderNumberProps = {
+  tag: string;
+  children: React.ReactNode;
+};
+
+const RenderNumber: React.FC<RenderNumberProps> = ({ tag, children }) => {
+  return (
+    <>
+      <div className="px-2 py-6 bg-slate-200 w-[90%] rounded-md">
+        <h1 className="font-bold">{tag}</h1>
+        <div>{children}</div>
+      </div>
+    </>
+  );
+};
+export default RenderNumber;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,8 @@
 import { useWeb3 } from "./components/providers/Provider";
 import Skeleton from "./components/ui/Skeleton";
 import useDataSet from "./components/hooks/useDataSet";
+import Image from "next/image";
+import RenderNumber from "./components/ui/RenderNumber";
 
 export type AllDonorInfo = {
   address: string | undefined;
@@ -36,7 +38,35 @@ const Home = () => {
   return (
     <div className="h-screen py-2 sm:px-10">
       <div className="flex flex-col space-y-2 md:flex-row md:space-x-2 md:space-y-0  items-center justify-between text-center mx-auto">
-        <div className="px-2 py-6 bg-slate-200 w-4/5 rounded-md">
+        <RenderNumber tag={"Donors"}>
+          {contractIsLoading ? (
+            <Skeleton height="4" width="10" />
+          ) : contract ? (
+            <p>{donorAddress?.length ?? ""}</p>
+          ) : (
+            <div>{pushToInstallWallet()}</div>
+          )}
+        </RenderNumber>
+        <RenderNumber tag={"Recipients"}>
+          {contractIsLoading ? (
+            <Skeleton height="4" width="20" />
+          ) : contract ? (
+            <p>{recipientAddress?.length ?? ""}</p>
+          ) : (
+            <div>{pushToInstallWallet()}</div>
+          )}
+        </RenderNumber>
+        <RenderNumber tag={"Matching(pair)"}>
+          {contractIsLoading ? (
+            <Skeleton height="4" width="20" />
+          ) : contract ? (
+            <p>{matchingNum ?? ""}</p>
+          ) : (
+            <div>{pushToInstallWallet()}</div>
+          )}
+        </RenderNumber>
+
+        {/* <div className="px-2 py-6 bg-slate-200 w-[90%] rounded-md">
           <h1 className="font-bold">Donor number</h1>
 
           {contractIsLoading ? (
@@ -46,8 +76,8 @@ const Home = () => {
           ) : (
             <div>{pushToInstallWallet()}</div>
           )}
-        </div>
-        <div className="px-2 py-6 bg-slate-200 w-4/5 rounded-md">
+        </div> */}
+        {/* <div className="px-2 py-6 bg-slate-200 w-[90%] rounded-md">
           <h1 className="font-bold">Recipient numbers</h1>
           {contractIsLoading ? (
             <Skeleton height="4" width="20" />
@@ -57,7 +87,7 @@ const Home = () => {
             <div>{pushToInstallWallet()}</div>
           )}
         </div>
-        <div className="px-2 py-6 bg-slate-200 w-4/5 rounded-md">
+        <div className="px-2 py-6 bg-slate-200 w-[90%] rounded-md">
           <h1 className="font-bold">Matching number(pair)</h1>
           {contractIsLoading ? (
             <Skeleton height="4" width="20" />
@@ -66,8 +96,15 @@ const Home = () => {
           ) : (
             <div>{pushToInstallWallet()}</div>
           )}
-        </div>
+        </div> */}
       </div>
+      <Image
+        src="/love.jpg"
+        alt="header"
+        width={1280}
+        height={500}
+        className="mt-2 w-[90%] sm:w-full mx-auto"
+      />
     </div>
   );
 };
